2a6bb02dcc43b99f04b1f515a8810293aee97bdc
aidenjangkkj <127905849+aidenjangkkj@users.noreply.github.com>
2024-11-23 02:26:31 +0900

    Delete client/sanityClient.js

--- client/sanityClient.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client';
-
-const client = sanityClient({
-  projectId: 'y0qr55ul',  // 자신의 프로젝트 ID로 변경
-  dataset: 'production',         // 데이터셋 이름 (보통 production)
-  useCdn: true,                  // 실시간 데이터의 경우 false로 설정
-});
-
-export default client;
